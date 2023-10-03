0efb456abf071a67bb4492d17df6227a19191693
gavin lasher <krgavin21@yahoo.com>
2023-10-03 07:45:41 -0600

    Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from './pages/SignUp';
 import Proflie from './pages/Proflie';
 import About from './pages/about';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/sign-in' element={<SignIn/>}/>
