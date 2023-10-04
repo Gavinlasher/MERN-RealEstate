7dbd54c3551ffdb327cff96c578d9985a83a1363
gavin lasher <krgavin21@yahoo.com>
2023-10-04 07:53:51 -0600

    sign up route

--- backend/Routes/auth.route.js
+++ backend/Routes/auth.route.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { signup } from '../Controller/auth.controller.js';
 
 const router = express.Router();
-router.post('/signup')
+router.post('/signup',signup)
+
+export default router
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './Routes/user.route.js';
+import authRouter from './Routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
     console.log("db is connected")
@@ -10,8 +11,10 @@ mongoose.connect(process.env.MONGO).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 app.listen(3000,() =>{
     console.log("Server is Running on Port 3000");
 });
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
